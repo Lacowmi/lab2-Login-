f482e9d8b230f2da3451d93d10a0fd959e508d4a
Lacowmi <55349470+Lacowmi@users.noreply.github.com>
2019-11-03 13:01:35 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,16 +1,12 @@
 <?php
   session_start();
-  ob_start();
 
   function checkAuth(string $login, string $password): bool
   {  
       $users = require __DIR__ . '/users.php';
       foreach ($users as $user) {
           if ($user['login'] === $login && $user['password'] === $password) {
-  
-              $_SESSION['userLang'] = $user['lang'];
-              $_SESSION['allLang'] = $users;
-              $_SESSION['userLogin'] = $login;
+              $_SESSION['user'] = $user;
               return true;
           }
       }
